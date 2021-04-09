64d8d2ffbbfff19dc2e8f28d9beeef669104fd6e
Kyle Bondo <gagglepod@gmail.om>
2021-04-09 18:52:10 -0400

    Added GraphQLID feature.

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,25 @@
 const graphql = require('graphql');
 const _= require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
 var books = [
     { name:'Name of the Wind', genre:'Fantasy', id:'1' },
     { name:'The Final Empire', genre:'Fantasy', id:'2' },
-    { name:'The Long Earth', genre:'Sci-Fi', id:'3' },
+    { name:'The Long Earth', genre:'Sci-Fi', id:'3' }
 ];
+
+var authors = [
+    { name:'Patrick Rothfuss', age: 44, id:'1' },
+    { name:'Brandon Sanderson', age: 42, id:'2' },
+    { name:'Terry Pratchett', age: 66, id:'3' }
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -24,7 +30,7 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString }},
+            args: { id: { type: GraphQLID }},
             resolve(parent, args){
                 // code to get data from db / other source
                 return _.find(books, { id: args.id });
