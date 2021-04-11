d8bbb508294fc50b3c6239764bd5c3228ee408c9
Kyle Bondo <gagglepod@gmail.om>
2021-04-11 15:22:14 -0400

    Apollo installed and configured.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
 //components
 import BookList from "./components/BookList";
 
+const client = new ApolloClient({
+  uri: 'http://127.0.0.1:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id="main">
       <h1>YardDog Reading List</h1>
       <BookList />
     </div>
+    </ApolloProvider>
   );
 }
 
